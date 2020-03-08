679b4789a59a07d11fcecdf58af6ee8d31cfb603
นาย ปัญจพล อ่อนโคทา <58883256+Punjapon@users.noreply.github.com>
2020-03-08 22:59:59 +0700

    100%

---
+++ src/com/company/Student.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Student
+{
+    String name = "นาย ปัญตพล อ่อนโคทา";
+    String id = "623410006-2";
+    int age = 2563-2544;
+    public void GetStudent()
+    {
+        System.out.println("ชื่อ-สกุล : " + name);
+        System.out.println("รหัสนักศึกษา : " + id);
+        System.out.println("อายุ " + age + " ปี");
+        System.out.println("สาขาวิทยาการคอมพิวเตอร์และสารสนเทศ");
+    }
+}
